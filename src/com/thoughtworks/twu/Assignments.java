a289f718bd15060901d101081095576a48239945
Xavier Naunay <xnaunay@thoughtworks.com>
2016-07-25 09:54:41 -0500

    Assignment 2 done.

--- src/com/thoughtworks/twu/Assignments.java
+++ src/com/thoughtworks/twu/Assignments.java
@@ -3,6 +3,18 @@ package com.thoughtworks.twu;
 public class Assignments {
 
     public static void main(String[] args) {
+        Assignments assignments = new Assignments();
+        assignments.asterisk();
+        assignments.horizontalLine(8);
+    }
+
+    private void asterisk() {
         System.out.println("*");
+    }
+
+    private void horizontalLine(int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.print("*");
+        }
     }
 }
