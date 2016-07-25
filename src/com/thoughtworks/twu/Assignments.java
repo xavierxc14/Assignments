b78568cd4112e334daca356a9fc487bcef2d022f
Xavier Naunay <xnaunay@thoughtworks.com>
2016-07-25 10:03:34 -0500

    Assignment 3 done.

--- src/com/thoughtworks/twu/Assignments.java
+++ src/com/thoughtworks/twu/Assignments.java
@@ -6,15 +6,39 @@ public class Assignments {
         Assignments assignments = new Assignments();
         assignments.asterisk();
         assignments.horizontalLine(8);
+        assignments.verticalLine(3);
     }
 
+    /**
+     * Easiest exercise ever.
+     * Print one asterisk to the console.
+     */
     private void asterisk() {
         System.out.println("*");
     }
 
+    /**
+     * Draw a horizontal line.
+     * Given a number n, print n asterisks on one line.
+     *
+     * @param n the number of asterisks to print.
+     */
     private void horizontalLine(int n) {
         for (int i = 0; i < n; i++) {
             System.out.print("*");
+        }
+        System.out.println();
+    }
+
+    /**
+     * Draw a vertical line.
+     * Given a number n, print n lines, each with one asterisks
+     *
+     * @param n the number of lines to print.
+     */
+    private void verticalLine(int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.println("*");
         }
     }
 }
